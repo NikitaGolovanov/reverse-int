a6b14aea6b11dda5af24d5c661d7d5bab09927c8
nikitagolovanov <119881857+NikitaGolovanov@users.noreply.github.com>
2023-01-13 17:01:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+        if (n<0) n=-n
+        var str=n.toString()
+        var arr=[]
+        for (let i=0; i<str.length; i++) {arr.push(str[i])}
+        return Number(arr.reverse().join(""))
 }
